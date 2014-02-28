9211e322923f59ff7ab574ca198a7f0d35038394
Travis Stroud <strou.dtravis@gmail.com>
2014-02-28 16:11:15 -0600

    better vm provisioner

--- vm/bootstrap.sh
+++ vm/bootstrap.sh
@@ -17,9 +17,18 @@ apt-get install -y apache2
 rm -rf /var/www
 ln -fs /srv /var/www
 
-apt-get install -y php5 libapache2-mod-php5 php5-cli php5-common php5-curl php5-mcrypt php5-mysql
+apt-get install -y php5 libapache2-mod-php5 php5-cli php5-common php5-curl php5-mcrypt php5-mysql php5-imagick
 
 apt-get install -y git
 apt-get install -y vim
+
+a2enmod rewrite
+
+mv /etc/apache2/envvars /etc/apache2/envvars.backup
+cp /srv/vm/envvars /etc/apache2
+rm -rf /var/lock/apache2
+
+mv /etc/apache2/sites-available/default /etc/apache2/sites-available/default.backup
+cp /srv/vm/default /etc/apache2/sites-available
 
 service apache2 restart
